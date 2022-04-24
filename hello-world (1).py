904ea802302b2cedaff869e7bc8ffda0dfc0692f
Gabriele Peck <91040074+symbolisticism@users.noreply.github.com>
2022-04-23 20:59:24 -0700

    added readme and a python hello world program

---
+++ hello-world (1).py
@@ -0,0 +1,12 @@
+# prompt the user for their favorite color and store
+#   it in the color variable
+color = input('Please type your favorite color: ')
+
+# print a message showing the user their favorite color
+print('Your favorite color is...(drum roll please)...', color, '!')
+
+# challenge the user to a memorizing contest
+message = input('I bet you I can memorize something really fast! Type something long in, and I will recite the whole thing back to you!\n')
+
+# print out the "memorized" message to the user
+print(message)
